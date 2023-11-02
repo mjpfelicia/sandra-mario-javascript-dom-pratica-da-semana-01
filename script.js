3ab7630e03c6bbb9c5eeccfb2cd7c46398fa1f74
Sandra Felicio <mjpfelicia@gmail.com>
2023-11-01 21:47:53 -0300

    execcultando exercicio

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 // Selecionar todos elementos da classe 'produto_preco'
 
+const arrayPrecos = document.getElementsByClassName("produto_preco");
+
 // Implementar um comando de repetição para analisar o valor 'innerText' de cada um dos elementos e somá-los (lembre-se de converter o valor para float)
 
-// Escrever no conteúdo da página o valor da soma 
+let total = 0;
 
+for (let index = 0; index < arrayPrecos.length; index++) {
+    const preco = arrayPrecos[index].innerText;
+    total +=  parseFloat(preco)
+    console.log(total)
+    
+}
+// Escrever no conteúdo da página o valor da soma 
+document.write("Total: R$ " +total)
