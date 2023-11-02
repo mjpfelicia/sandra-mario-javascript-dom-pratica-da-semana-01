f4286227090330ac6e5c7a4972132daa4aaa6b5a
mjpfelicia <86963473+mjpfelicia@users.noreply.github.com>
2023-11-02 00:12:10 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Selecionar todos elementos da classe 'produto_preco'
+
+// Implementar um comando de repetição para analisar o valor 'innerText' de cada um dos elementos e somá-los (lembre-se de converter o valor para float)
+
+// Escrever no conteúdo da página o valor da soma 
+
