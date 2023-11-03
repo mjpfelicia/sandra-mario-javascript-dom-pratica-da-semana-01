03d0154b31000cb8f118c5467be68427b16429b8
Sandra Felicio <mjpfelicia@gmail.com>
2023-11-02 21:01:05 -0300

    modificando exercicio para alinha com o do mario

--- script.js
+++ script.js
@@ -1,16 +1,15 @@
 // Selecionar todos elementos da classe 'produto_preco'
+const precos = document.querySelectorAll('.produto_preco');
 
-const arrayPrecos = document.getElementsByClassName("produto_preco");
+// Inicializar uma iável para armazenar a soma
+let soma = 0;
 
 // Implementar um comando de repetição para analisar o valor 'innerText' de cada um dos elementos e somá-los (lembre-se de converter o valor para float)
-
-let total = 0;
-
-for (let index = 0; index < arrayPrecos.length; index++) {
-    const preco = arrayPrecos[index].innerText;
-    total +=  parseFloat(preco)
-    console.log(total)
-    
+for ( i = 0; i < precos.length; i++) {
+  const precoTexto = precos[i].innerText; // Obter Preço (txt)
+ const precoNumero = parseFloat(precoTexto); // Convertendo Texto em Número (Ponto Flutuante)
+  soma += precoNumero; // Somando Preço (à Soma)
 }
-// Escrever no conteúdo da página o valor da soma 
-document.write("Total: R$ " +total)
+
+// Escrever no conteúdo da página o valor da soma
+document.write(`<p>Total: R$ ${soma.toFixed(2)} </p>`)
