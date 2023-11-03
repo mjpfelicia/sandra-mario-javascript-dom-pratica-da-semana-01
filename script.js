b8335dc60211c2ca2ae238098547c3b2f1b46102
Sandra Felicio <mjpfelicia@gmail.com>
2023-11-02 21:08:31 -0300

    modificando

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Selecionar todos elementos da classe 'produto_preco'
 const precos = document.querySelectorAll('.produto_preco');
 
-// Inicializar uma iável para armazenar a soma
+// Inicializar uma variável para armazenar a soma
 let soma = 0;
 
 // Implementar um comando de repetição para analisar o valor 'innerText' de cada um dos elementos e somá-los (lembre-se de converter o valor para float)
 for ( i = 0; i < precos.length; i++) {
-  const precoTexto = precos[i].innerText; // Obter Preço (txt)
- const precoNumero = parseFloat(precoTexto); // Convertendo Texto em Número (Ponto Flutuante)
+    const precoTexto = precos[i].innerText; // Obter Preço (txt)
+    const precoNumero = parseFloat(precoTexto); // Convertendo Texto em Número (Ponto Flutuante)
   soma += precoNumero; // Somando Preço (à Soma)
 }
 
